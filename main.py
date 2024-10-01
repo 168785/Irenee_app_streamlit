c3bcf85b88454ff7a1d2be03969381b70f4a91c8
168785 <murhula.mugwarhiriza@strathmore.edu>
2024-10-01 19:21:59 +0300

    hello

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import streamlit as st
 import joblib
 import numpy as np
-# Charger le modèle pré-entraîné
+# Load the pre-trained model
 model = joblib.load("insurance.pkl")
 
-# Titre de l'application
-st.title('Prédiction du Coût de l\'Assurance Santé')
+# Application title
+st.title('Health Insurance Cost Prediction')
 
-# Description de l'application
+# App description
 st.write('''
-Cette Applicatiion predit le cout de l'assurance sante en fonction de l'age, sex, bmi, region, children and smoker
+This App predicts the cost of health insurance based on age, sex, bmi, region, children and smoker
 ''')
-# creer  des sliders et des options pour les inputs de l'utilisation
+# create sliders and options for usage inputs
 age = st.number_input('Age', 1, 100, 39)
 sex = st.selectbox('Sex', ('female', 'male'))
 bmi = st.number_input('BMI', 15.00, 53.00, 30.00 )
 children = st.number_input('Children', 0, 5, 1)
 smoker = st.selectbox('Smoker', ('yes', 'no'))
 region = st.selectbox('Region', ('southwest', 'southeast', 'northwest', 'northeast'))
-# convertir les inputs en format numeriques
-# convertir la collone sex
+# convert inputs into numeric format
+# convert sex column
 if sex == 'male':
     sex = 1
 elif sex == 'female':
     sex = 0
-# convertie la collone smoker
+# converted the smoker column
 if smoker == 'yes':
     smoker = 1
 elif smoker == 'no':
     smoker = 0
-# convertir la collone region
+# converted the region column
 if region == 'southwest':
     region = 3
 elif region == 'southeast':
@@ -38,8 +38,8 @@ elif region == 'northwest':
     region = 1
 elif region == 'northeast':
     region  = 0
-# afficher les inputs saisis par l'utilisateur
-st.subheader('Vos informations:')
+# display inputs entered by the user
+st.subheader('Your information:')
 st.write(f'Age = {age}')
 st.write(f'Sex = {'male' if sex == 1 else 'female'}')
 st.write(f'BMI = {bmi}')
@@ -53,12 +53,12 @@ elif region == 1:
     st.write(f'Region = {'northwest'}')
 elif region == 0:
     st.write(f'Region = {'northeast'}')
-# creer une prediction quand l'utilisateur appuie sur le boutton
+# create a prediction when the user presses the button
 if st.button('Predire le cout de charge'):
-    # preparer les donnees d'entrees pour le modele
+    # prepare input data for the model
     input_data = np.array([[age, sex, bmi, children, smoker, region]])
-    # faire la prediction
+    # make the prediction
     predicted_cost = model.predict(input_data)
-    # afficher le resultat
+    # show result
     st.subheader(f"{predicted_cost[0]}")
 
